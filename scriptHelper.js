fa1dfbc4fede67a616707ab315a3eb714d359ffd
Vit-Knysh <v,knysh85@gmail.com>
2022-06-20 23:02:56 -0400

    Added Fetch function

--- scriptHelper.js
+++ scriptHelper.js
@@ -27,11 +27,10 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then((response) => response.json());
 
     return planetsReturned;
-}
+};
 
 function pickPlanet(planets) {
 }
