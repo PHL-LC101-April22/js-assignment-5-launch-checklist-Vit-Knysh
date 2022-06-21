e329070f12753390c32b09c0a906c77532f5ec0a
Vit-Knysh <v,knysh85@gmail.com>
2022-06-20 23:08:10 -0400

    Added listed planets

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { myFetch } = require("./scriptHelper");
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
@@ -12,6 +12,16 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
+       let pickedPlanet = pickPlanet (listedPlanets);
+       addDestinationInfo (
+        document,
+        pickedPlanet.name,
+        pickedPlanet.diameter,
+        pickedPlanet.star,
+        pickedPlanet.distance,
+        pickedPlanet.moons,
+        pickedPlanet.image
+       );
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
